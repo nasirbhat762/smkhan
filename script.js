0794fdd7208e65c12fc4e62d7099e732be7f66e8
nasirbhat762 <nasirbhat762@gmail.com>
2024-06-03 22:40:54 +0530

    updated services

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  const isVisible =
-    rect.top >= window.pageYOffset &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) +
-        window.scrollY &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+// services data 
+let servicesData = [
+  { heading: "h1", para: "p1", link: "abc" },
+  { heading: "h2", para: "p2", link: "def" },
+  { heading: "h3", para: "p3", link: "ghi" },
+  { heading: "h4", para: "p4", link: "jkl" },
+  { heading: "h5", para: "p5", link: "mno" },
+  { heading: "h6", para: "p6", link: "pqr" },
+  { heading: "h7", para: "p7", link: "stu" },
+  { heading: "h8", para: "p8", link: "vwx" },
+  { heading: "h9", para: "p9", link: "yz" },
+  { heading: "h10", para: "p10", link: "abc123" },
+];
+// getting html element 
+let servies = document.getElementById("services");
 
-  return isVisible;
+// changing inner html of services
+let text = ""
+for(let item of servicesData){
+    
+    text =  text + `<div class="card  text-center" style="width: 18rem;">
+    <i class="bi bi-airplane"></i>
+    <div class="card-body">
+      <h5 class="card-title">${item.heading}</h5>
+      <p class="card-text">${item.para}</p>
+      <a href="${item.link}" class="btn btn-danger rounded-0">Get more info</a>
+    </div>
+  </div>`;
+    console.log(text)
+    services.innerHTML =  text
 }
 
-function addAnimation() {
-  // Check if the footer element is in the viewport
-  let footers = document.getElementsByTagName("footer");
-  let footer = footers[0]; // Assuming there's only one footer element
 
-  if (isInViewport(footer)) {
-    console.log("Footer is in the viewport!"); // Placeholder for animation trigger
-    // Add your animation logic here
-    footer.classList.add("footer-animation");
-  }
-}
-
-// Listen for scroll events on the window
-// window.addEventListener("scroll", addAnimation);
+// fading animation 
 window.onload = function () {
   let header = document.getElementById("header");
   header.classList.add("header-animation");
